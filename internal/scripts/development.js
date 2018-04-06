21563d7502607fb645f3009763cd343257c4577f
Benoit Maigret <benoit.maigret@fairfaxmedia.com.au>
2018-04-06 15:12:57 +1000

    Init commit

---
+++ internal/scripts/development.js
@@ -0,0 +1,18 @@
+import webpack from 'webpack'
+import webpackDevServer from 'webpack-dev-server'
+
+import config from '../webpack/webpack.config.babel'
+
+const options = {
+  contentBase: './dist',
+  hot: true,
+  host: 'localhost'
+}
+
+webpackDevServer.addDevServerEntrypoints(config, options)
+const compiler = webpack(config)
+const server = new webpackDevServer(compiler, options)
+
+server.listen(5000, 'localhost', () => {
+  console.log('dev server listening on port 5000')
+})
---
+++ internal/webpack/webpack.config.babel.js
@@ -0,0 +1,44 @@
+import path from 'path'
+import webpack from 'webpack'
+import HtmlWebPackPlugin from 'html-webpack-plugin'
+
+export default {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.[hash].js',
+    publicPath: '/'
+  },
+  // resolve: {
+  //   modules: [
+  //     path.join(__dirname, './src'),
+  //     path.join(__dirname, './node_modules')
+  //   ],
+  //   alias: {
+  //     src: path.join(__dirname, 'src')
+  //   }
+  // },
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
